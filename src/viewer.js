f039c177c0909ead552e56448dcbf2d93a72e68e
Jeff Dyer <jeffdyer@acm.org>
2016-11-16 12:15:00 -0700

    Add react import

--- src/viewer.js
+++ src/viewer.js
@@ -2,6 +2,7 @@
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 /* Copyright (c) 2015, Jeff Dyer, Art Compiler LLC */
 import {assert, message, messages, reserveCodeRange} from "./assert.js";
+import * as React from "react";
 window.gcexports.viewer = (function () {
   function update(el, obj, src, pool) {
     var data = JSON.parse(obj).data;
